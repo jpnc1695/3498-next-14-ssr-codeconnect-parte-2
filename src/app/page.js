ff5ddfeb72e025cff7ac9028131b27d41a16773a
Vinicios Neves <vinicios_neves@hotmail.com>
2024-02-07 19:30:20 +0000

    Video 5.1

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,21 @@ import styles from './page.module.css'
 import Link from "next/link"
 import db from "../../prisma/db"
 
-async function getAllPosts (page) {
+async function getAllPosts (page, searchTerm) {
   try {
+
+    const where = {}
+
+    if (searchTerm) {
+      where.title = {
+        contains: searchTerm,
+        mode: 'insensitive'
+      }
+    }
 
     const perPage = 4;
     const skip = (page - 1) * perPage;
-    const totalItems = await db.post.count()
+    const totalItems = await db.post.count({ where })
     const totalPages = Math.ceil(totalItems / perPage)
     const prev = page > 1 ? page - 1 : null 
     const next = page < totalPages ? page + 1 : null
@@ -18,6 +27,7 @@ async function getAllPosts (page) {
     const posts = await db.post.findMany({
       take: perPage,
       skip,
+      where,
       orderBy: { createdAt: 'desc' },
       include: {
         author: true
@@ -34,7 +44,8 @@ async function getAllPosts (page) {
 
 export default async function Home({ searchParams }) {
   const currentPage = parseInt(searchParams?.page || 1)
-  const { data: posts, prev, next } = await getAllPosts(currentPage)
+  const searchTerm = searchParams?.q
+  const { data: posts, prev, next } = await getAllPosts(currentPage, searchTerm)
   return (
     <main className={styles.grid}>
       {posts.map(post =>  <CardPost key={post.id} post={post} />)}
