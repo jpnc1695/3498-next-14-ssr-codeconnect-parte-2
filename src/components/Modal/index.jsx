15117724a311cf1d0af1b3f974e86ba4e7fdc894
jpnc1695 <joaopaulonunescosta@gmail.com>
2025-02-04 16:22:45 -0300

    Criacao Modal para comentários.

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import styles from './modal.module.css'
+
+const Modal = forwardRef(({ children, ref }) => {
+  const dialogRef = useRef(null);
+
+  const closeModal = () => {
+    dialogRef.current.close();
+  };
+
+  const openModal = () => {
+    dialogRef.current.showModal();
+  };
+
+  useImperativeHandle(ref, () => {
+    return {
+      closeModal,
+      openModal,
+    };
+  });
+
+  return (
+    <dialog ref={dialogRef}>
+      <header className={styles.header}>
+        <button onClick={closeModal}>X</button>
+      </header>
+      {children}
+    </dialog>
+  );
+});
