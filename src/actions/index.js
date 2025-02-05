afdc73e55e303c11e1bbea0e6de6e4efb833dea1
jpnc1695 <joaopaulonunescosta@gmail.com>
2025-02-05 15:02:56 -0300

    finalizando os modals e caixa de comentário

--- src/actions/index.js
+++ src/actions/index.js
@@ -21,3 +21,26 @@ export async function incrementThumbsUp(post) {
     revalidatePath('/')
     revalidatePath(`/${post.slug}`)
 }
+
+
+export async function postComment(post, formData) {
+    
+    const author = await db.user.findFirst({
+        where:{
+            userName: "anabeatriz_dev"
+        }
+    })
+
+    await db.comment.create({
+        data:{
+            text: formData.get('text'),
+            authorId: author.id,
+            postId: post.id
+        }
+    })
+
+    console.log(formData)
+
+    revalidatePath('/')
+    revalidatePath(`/${post.slug}`)
+}
--- src/components/ModalComment/index.jsx
+++ src/components/ModalComment/index.jsx
@@ -1,16 +1,29 @@
 "use client";
+import { useRef } from "react"; 
 import { IconButton } from "../IconButton";
 import { Modal } from "../Modal";
 import { Chat } from "../icons/Chat";
-import { useRef } from "react"; 
+import { Textarea } from "../Textarea";
+import { Subheading} from "../SubmitHeading";
+import { SubmitButton } from "../SubmitButton";
+import styles from './commentmodal.module.css'
 
-export const ModalComment = () => {
+
+export const ModalComment = ({action}) => {
   const modalRef = useRef(null);
 
   return (
     <>
       <Modal ref={modalRef}>
-        <h1>Olá meu mano !!!</h1>
+        <form action={action} onSubmit={()=> modalRef.current.closeModal()}>
+          <Subheading>Deixe seu comentário sobre o Post:</Subheading>
+          <Textarea name="text" rows={8} placeholder="Digite aqui..."/>
+          <div className={styles.footer}>
+              <SubmitButton>
+                Comentar
+              </SubmitButton>
+          </div>         
+        </form>
       </Modal>
       <IconButton onClick={() => modalRef.current.openModal()}>
         <Chat />
